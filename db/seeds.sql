4a81512750c7a9f056c30caa192c46c7e4ed13be
johnnyowen <j.owen1993@outlook.com>
2023-06-11 13:08:20 -0700

    finished per homework criteria, will add comments and hopefully BONUS later

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,25 +1,25 @@
-INSERT INTO department (id, dep_name)
-VALUES  (1, 'Management'),
-        (2, 'Sales'),
-        (3, 'IT'),
-        (4, 'Accounting');
+INSERT INTO department (dep_name)
+VALUES  ('Management'),
+        ('Sales'),
+        ('IT'),
+        ('Accounting');
 
-INSERT INTO roles (id, title, salary, department_id)
-VALUES  (01, 'General Manager', 150000, 1),
-        (02, 'Assiatant Manager', 120000, 1),
-        (03, 'Sales Lead', 90000, 2),
-        (04, 'Sales Representative', 75000, 2),
-        (05, 'IT Lead', 100000, 3),
-        (06, 'IT Technician', 85000, 3),
-        (07, 'Accounting Lead', 95000, 4),
-        (08, 'Accounting Assistant', 80000, 4);
+INSERT INTO roles (title, salary, department_id)
+VALUES  ('General Manager', 150000, 1),
+        ('Assiatant Manager', 120000, 1),
+        ('Sales Lead', 90000, 2),
+        ('Sales Representative', 75000, 2),
+        ('IT Lead', 100000, 3),
+        ('IT Technician', 85000, 3),
+        ('Accounting Lead', 95000, 4),
+        ('Accounting Assistant', 80000, 4);
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES  (001, 'Masayoshi', 'Takanaka', 01, null),
-        (002, 'Fenne', 'Lily', 03, null),
-        (003, 'DJ', 'Gigola', 05, null), 
-        (004, 'Kev', 'Koko', 07, null),
-        (005, 'Libby', 'Lux', 02, 001),
-        (006, 'Lana', 'Del Rey', 04, 002),
-        (007, 'James', 'Bond', 06, 003),
-        (008, 'Gioli', 'Assia', 08, 004);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ('Masayoshi', 'Takanaka', 1, null),
+        ('Fenne', 'Lily', 3, null),
+        ('DJ', 'Gigola', 5, null), 
+        ('Kev', 'Koko', 7, null),
+        ('Libby', 'Lux', 2, 1),
+        ('Lana', 'Del Rey', 4, 2),
+        ('James', 'Bond', 6, 3),
+        ('Gioli', 'Assia', 8, 4);
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const cTable = require('console.table');
-const inquier = require('inquirer');
-const mysql = require('mysql');
-const asciiart = require('asciiart-logo')
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
 require('dotenv').config();
+const logo = require('asciiart-logo');
+const config = require('./package.json');
+console.log(logo(config).render());
 
-const db = mysql.createConnection(
+
+const connection = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root',
@@ -12,4 +15,210 @@ const db = mysql.createConnection(
         database: 'employee_tracker_db'
     },
     console.log(`Connected to employee_tracker_db`)
-)
+);
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log('connected as id ' + connection.threadId);
+    runEmpTracker();
+  });
+
+const questions = [{
+    type: "list",
+    name: "what",
+    message: "What would you like to do?",
+    choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update An Employee Role', 'Exit']
+}];
+
+function runEmpTracker () { 
+    inquirer.prompt(questions)
+    .then((answer) => {
+        if (answer.what === 'View All Departments') {
+            viewAllDepartments();
+        } else if (answer.what === 'View All Roles') {
+            viewAllRoles();
+        } else if (answer.what === 'View All Employees') {
+            viewAllEmployees();
+        } else if (answer.what === 'Add A Department') {
+            addDepartment();
+        } else if (answer.what === 'Add A Role') {
+            addRole();
+        } else if (answer.what === 'Add An Employee') {
+            addEmployee();
+        } else if (answer.what === 'Update An Employee Role') {
+            updateEmployeeRole();
+        } else if (answer.what === 'Exit') {
+            console.log('You have exited Employee Tracker, goodbye!');
+            process.exit();
+        }
+    });
+};
+
+function viewAllDepartments() {
+    console.log('Viewing All Departments');
+    connection.query('SELECT * FROM department', (err, results) => {
+            console.table(results);
+            runEmpTracker();
+        }
+      );
+};
+
+function viewAllRoles() {
+    console.log('Viewing All Roles');
+    connection.query('SELECT * FROM roles', (err, results) => {
+        console.table(results); 
+        runEmpTracker();
+      }
+    );
+};
+
+function viewAllEmployees() {
+    console.log('Viewing All Employees');
+    connection.query('SELECT * FROM employee', (err, results) => {
+        console.table(results); 
+        runEmpTracker();
+      }
+    );
+};
+
+function addDepartment() {
+    inquirer.prompt({
+        type: 'input',
+        name: 'dept',
+        message: 'What is the name of the new Department?'
+    })
+    .then((answer) => {
+        connection.query('INSERT INTO department SET ?',{dep_name: answer.dept}, (err, results) => {
+            viewAllDepartments();
+            console.log('Successfully Added A Department');
+        });
+    });
+};
+
+function addRole() {
+    const departments = [];
+    connection.query('SELECT * FROM department', (err, res) => {
+        res.forEach(dep => {
+            let depObj = {
+                name: dep.dep_name,
+                value: dep.id
+            };
+            departments.push(depObj);
+        });
+        inquirer.prompt([
+                {
+                type: 'input',
+                name: 'roleTitle',
+                message: 'What is the name of the new Role?'
+            },{
+                type: 'input',
+                name: 'roleSalary',
+                message: 'What is the salary of the new Role?'
+            },{
+                type: 'list',
+                name: 'roleDep',
+                message: 'What Department is the new Role in?',
+                choices: departments
+            }
+        ])
+        .then((answer) => {
+            connection.query('INSERT INTO roles SET ?',{title: answer.roleTitle, salary: answer.roleSalary, department_id: answer.roleDep}, (err, results) => {
+                viewAllRoles();
+                console.log('Successfully Added A Role');
+            });
+        });
+    });
+};
+
+function addEmployee() {
+    connection.query('SELECT * FROM employee', (err, empRes) =>{
+        const managers = [
+            {
+                name:'None',
+                value: 0
+            }
+        ];
+        empRes.forEach(({ first_name, last_name, id }) => {
+            managers.push({
+                name: first_name + " " + last_name,
+                value: id
+            });
+        });
+        connection.query('SELECT * FROM roles', (err, roleRes) => {
+            const roles = [];
+            roleRes.forEach(({ title, id }) => {
+                roles.push({
+                    name: title,
+                    value: id
+                });
+            });
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'empFirstName',
+                    message: 'What is the first name of the added Employee?'
+                },{
+                    type: 'input',
+                    name: 'empLastName',
+                    message: 'What is the last name of the added Employee?'
+                },{
+                    type: 'list',
+                    name: 'empRole',
+                    message: 'What is the role of the added Employee?',
+                    choices: roles
+                },{
+                    type: 'list',
+                    name: 'empManager',
+                    message: 'Who is the manager of the added Employee if applicable?',
+                    choices: managers
+                }
+            ])
+            .then((answers) => {
+                connection.query('INSERT INTO employee SET ?', {first_name: answers.empFirstName, last_name: answers.empLastName, role_id: answers.empRole, manager_id: answers.empManager}, (err, res) => {
+                    viewAllEmployees();
+                    console.log('Successfully Added An Employee');
+                });
+            });
+        });
+    });
+};
+
+function updateEmployeeRole() {
+    connection.query('SELECT * FROM employee', (err, empRes) => {
+        const empChoice = [];
+        empRes.forEach(({ first_name, last_name, id }) => {
+            empChoice.push({
+                name: first_name + " " + last_name,
+                value: id
+            });
+        });
+        connection.query('SELECT * FROM roles', (err, roleRes) => {
+            const roles = [];
+             roleRes.forEach(({ title, id}) => {
+                roles.push({
+                    name: title,
+                    value: id
+                });
+             });
+             inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'employee',
+                    message: 'Which employee would you like to update?',
+                    choices: empChoice
+                },{
+                    type: 'list',
+                    name: 'role',
+                    message: 'What new role does this employee have?',
+                    choices: roles
+                }
+             ])
+             .then((answers) => {
+                connection.query(`UPDATE employee SET role_id = ${answers.role} WHERE id = ${answers.employee}`, (err, res) => {
+                    viewAllEmployees();
+                    console.log('Successfully Updated An Employee');
+                });
+             });
+        });
+    });
+};
