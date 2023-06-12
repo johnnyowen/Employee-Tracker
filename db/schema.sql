39b23dd7aabb7b51bf9a4550cb7a70ccd2820d5c
johnnyowen <j.owen1993@outlook.com>
2023-06-12 14:34:58 -0700

    fixing typo

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,7 @@
 DROP DATABASE IF EXISTS employee_tracker_db;
 CREATE DATABASE employee_tracker_db;
 
--- Usinf said Database
+-- Using said Database
 USE employee_tracker_db;
 
 -- Creating the Tables
